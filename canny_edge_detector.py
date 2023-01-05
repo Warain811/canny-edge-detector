37eb41e0bab8c17014c9fcd8a780dfe597187d56
murky0610 <murkymurky0@gmail.com>
2023-01-05 17:49:24 +0800

    Hello

--- canny_edge_detector.py
+++ canny_edge_detector.py
@@ -10,12 +10,12 @@ class edgeDetection():
         self.totalNumberOfObjects = 0
         self.numberOfObjects = 0
 
-    def detectEdges(self, filepath):
+    def detectEdges(self, filepath,canny_scalex,canny_scaley):
         im = cv2.imread(filepath)
         imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
         imgray = cv2.GaussianBlur(imgray, (5, 5), 0)
-        detectedEdges = cv2.Canny(imgray, 0, 70, L2gradient= True, apertureSize=3) # use canny edge detection algorithm to find the outlines of objects in images
+        detectedEdges = cv2.Canny(imgray, canny_scalex,canny_scaley, L2gradient= True, apertureSize=3) # use canny edge detection algorithm to find the outlines of objects in images
         dilatedEdges = cv2.blur(detectedEdges, (3, 3), 0)
 
         contours, hierarchy = cv2.findContours(dilatedEdges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
